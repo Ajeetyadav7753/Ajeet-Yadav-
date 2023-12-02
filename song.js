9a66a1b8859a6a24199e94ea3b017fe25f446c48
Ajeetyadav7753 <152722922+Ajeetyadav7753@users.noreply.github.com>
2023-12-02 22:57:24 +0530

    Add files via upload

---
+++ song.js
@@ -0,0 +1,27 @@
+ let songs = [
+    {
+        name: 'kalkatiya raja',
+        path: 'song/1.mp3',
+        artist: 'Pawan Singh',
+    },
+    {
+        name: 'kallo',
+        path: 'song/2.mp3',
+        artist: 'Komal Choudhary, Harjeet Deewana',
+    },
+    {
+        name: 'radha teri chunari',
+        path: 'song/3.mp3',
+        artist: 'Shreya Ghoshal',
+    },
+    {
+        name: '52 ghaj',
+        path: 'song/4.mp3',
+        artist: 'Renuka Panwar',
+    },
+    {
+        name: 'dilwale',
+        path: 'song/5.mp3',
+        artist: 'Antara Mitra, Arijit Singh',
+    },
+]
